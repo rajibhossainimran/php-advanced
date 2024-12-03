085ef40ec1879eef6155f7be2be81d90e77d8815
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-03 23:52:43 +0600

    display time when click btn

--- dateTime/time.php
+++ dateTime/time.php
@@ -1,4 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>date and time</title>
+</head>
+<body>
+    <section>
+        <form action="" method="post">
+            <button type="submit" name="getTime">display time</button>
+        </form>
+    </section>
+</body>
+</html>
+
 <?php
-$date = new DateTime("now", new DateTimeZone("Asia/Dhaka"));
-echo $date->format("Y-m-d H:i:s");
+if(isset($_POST['getTime'])){
+    $date = new DateTime("now", new DateTimeZone("Asia/Dhaka"));
+     echo $date->format("Y-m-d H:i:s");
+}
 ?>
